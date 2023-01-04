292772e6c036cd2022df0cdfc2cf22e608a11534
moya <moya-hch@qq.com>
2023-01-04 16:43:37 +0800

    cache

--- x-message/src/main/java/com/xboot/message/core/annotation/MqConsumer.java
+++ x-message/src/main/java/com/xboot/message/core/annotation/MqConsumer.java
@@ -1,7 +1,7 @@
 package com.xboot.message.core.annotation;
 
 import com.xboot.message.constant.SendType;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
 import java.lang.annotation.*;
